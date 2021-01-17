f8b8f67ce0cb512345122b168b45961ef719333d
u-senanayake <mailtoudayanga@gmail.com>
2021-01-17 17:03:23 +0900

    Buttons with Grid layout

---
+++ app/src/main/java/senanayake/udayanga/com/section4video/GridLayoutActivity.java
@@ -0,0 +1,14 @@
+package senanayake.udayanga.com.section4video;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GridLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_layout);
+    }
+}
