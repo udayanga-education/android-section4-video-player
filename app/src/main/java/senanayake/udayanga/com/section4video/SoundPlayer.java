96897c78d5f4c1bf1f15ec62896e5b295c619567
u-senanayake <mailtoudayanga@gmail.com>
2021-01-17 12:21:31 +0900

    Added volume controller

--- app/src/main/java/senanayake/udayanga/com/section4video/SoundPlayer.java
+++ app/src/main/java/senanayake/udayanga/com/section4video/SoundPlayer.java
@@ -2,19 +2,52 @@ package senanayake.udayanga.com.section4video;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class SoundPlayer extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
+    SeekBar volumeController;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound_player);
         mediaPlayer = MediaPlayer.create(this, R.raw.sound);
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        volumeController = (SeekBar) findViewById(R.id.seekBar);
+        volumeController.setMax(maxVolume);
+        volumeController.setProgress(currentVolume);
+
+        volumeController.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("Volume ", Integer.toString(progress));
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
     public void playAction(View view) {
